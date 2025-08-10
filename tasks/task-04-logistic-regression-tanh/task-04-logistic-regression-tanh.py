cd84c574b6aa97745ef83427058d151de2095cb0
José Victor <josevictor.vd25@gmail.com>
2025-08-10 19:31:50 -0300

    task  4 feita

--- tasks/task-04-logistic-regression-tanh/task-04-logistic-regression-tanh.py
+++ tasks/task-04-logistic-regression-tanh/task-04-logistic-regression-tanh.py
@@ -11,48 +11,42 @@ class LogisticNeuron:
         self.loss_history = []
 
     def tanh(self, z):
-        ### START CODE HERE ###
-        ### TODO: implement the tanh activation
-        a = None
-        ### END CODE HERE ###
+        ### implement the tanh activation
+        a = (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
         return a
 
     def predict_proba(self, X):
-        ### START CODE HERE ###
-        ### TODO: compute activation output using tanh
-        z = None
-        a = None
-        ### END CODE HERE ###
+        ### compute activation output using tanh
+        z = X @ self.weights + self.bias
+        a = self.tanh(z)
         return a
 
     def predict(self, X):
-        prediction = None
+        prediction = self.predict_proba(X)
         return prediction
 
     def train(self, X, y):
-        ### START CODE HERE ###
-        ### TODO: convert y from {0, 1} to {-1, +1}
-        y_tanh = y
+        ### convert y from {0, 1} to {-1, +1}
+        y_tanh = 2 * y - 1
 
         for _ in range(self.epochs):
             # Forward pass
-            y_pred = None
+            y_pred = self.predict(X)
 
             # Compute error
-            error = None
+            error = np.mean(np.square(y_tanh - y_pred))
 
             # Gradients
-            grad_w = None
-            grad_b = None
+            grad_w = (2/X.shape[0]) * X.T @ (y_pred-y_tanh)
+            grad_b = (2/X.shape[0]) * np.sum(y_pred-y_tanh)
 
             # Update parameters
-            self.weights = None
-            self.bias = None
+            self.weights -= self.learning_rate * grad_w
+            self.bias -= self.learning_rate * grad_b
 
             # Compute MSE loss
-            loss = None
+            loss = error
             self.loss_history.append(loss)
-        ### END CODE HERE ###
 
 def generate_dataset():
     X, y = make_blobs(n_samples=200, centers=2, random_state=42, cluster_std=2.0)
